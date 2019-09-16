12b83ad923708b0d1e5acdf1fff4d4fee5569586
Cheryl Toh <e0035693@u.nus.edu>
2019-09-16 16:34:16 +0800

    Create Data Classes

---
+++ src/NUSMed-WebApp/Classes/BLL/DataBLL.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUSMed_WebApp.Classes.DAL;
+
+namespace NUSMed_WebApp.Classes.BLL
+{
+  public class DataBLL
+  {
+    private readonly DataDAL dataDAL = new DataDAL();
+
+  }
+}
---
+++ src/NUSMed-WebApp/Classes/DAL/DataDAL.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NUSMed_WebApp.Classes.DAL
+{
+  public class DataDAL : DAL
+  {
+    public DataDAL() : base() { }
+
+
+  }
+}
