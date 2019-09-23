a6e29c23ff2574ae046d17d71eb60c5bdfb4f205
dingheng4448 <ldh_1295@hotmail.com>
2019-09-23 18:57:15 +0800

    Update register device method

--- src/NUSMed-WebApp/API/AccountController.cs
+++ src/NUSMed-WebApp/API/AccountController.cs
@@ -78,8 +78,9 @@ namespace NUSMed_WebApp.API
                     {
                         accountBLL.MFADeviceIDUpdateFromPhone(nric, tokenID, deviceID);
 
+                        string test = nric + "::" + tokenID + "::" + "deviceID";
                         // what should server return upon successful registration?
-                        response = Request.CreateResponse(HttpStatusCode.OK);
+                        response = Request.CreateResponse(HttpStatusCode.OK, test);
                     }
                     catch
                     {
