db2910d0dcf93b6e5238024379987c58cf85499e
Sean Yap <e0177170@u.nus.edu>
2019-09-27 18:27:14 +0800

    Add exception handling for admin generate data functionality

--- src/NUSMed-WebApp/Admin/Researcher-Data.aspx.cs
+++ src/NUSMed-WebApp/Admin/Researcher-Data.aspx.cs
@@ -19,15 +19,15 @@ namespace NUSMed_WebApp.Admin
 
         protected void LinkbuttonDataGenerate_Click(object sender, EventArgs e)
         {
-            //try
-            //{
+            try
+            {
                 dataBLL.InsertAnonymizedTableToDb();
                 ScriptManager.RegisterStartupScript(this, GetType(), "alert", "toastr['success']('The database have been re-populated with Anonymised Records.');", true);
-            //}
-            //catch
-            //{
-            //    ScriptManager.RegisterStartupScript(this, GetType(), "alert", "toastr['error']('Error occured when Anonymising Records.');", true);
-            //}
         }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "alert", "toastr['error']('Error occured when Anonymising Records.'"+ex+");", true);
+        }
+    }
     }
 }
