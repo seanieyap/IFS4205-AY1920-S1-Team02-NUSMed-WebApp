2373c5b26333e1eb74672e7a8440d3efe5f32c44
dingheng4448 <ldh_1295@hotmail.com>
2019-10-26 01:10:51 +0800

    Refactor WebLogin code

--- src/NUSMed-WebApp/API/AccountController.cs
+++ src/NUSMed-WebApp/API/AccountController.cs
@@ -253,89 +253,23 @@ namespace NUSMed_WebApp.API
         }
 
         // POST api/account/weblogin
+        // Validates and authorizes client's web login request 
         [Route("weblogin")]
         [HttpPost]
-        public HttpResponseMessage WebLogin([FromBody]dynamic credentials)
-        {
-            var response = Request.CreateResponse(HttpStatusCode.Unauthorized);
-            string deviceID = credentials.deviceID;
-            string tokenID = credentials.tokenID;
-            string jwt = credentials.jwt;
-            //string guid = credentials.guid;
-
-            JWTBLL jwtBll = new JWTBLL();
-
-            if (!string.IsNullOrEmpty(jwt) && AccountBLL.IsDeviceIDValid(deviceID) && AccountBLL.IsTokenIDValid(tokenID))
-            {
-                if (jwtBll.ValidateJWT(jwt))
-                {
-                    string retrievedNRIC = jwtBll.getNRIC(jwt);
-
-                    // validate deviceID and tokenID for a nric
-                    if (accountBLL.IsValid(retrievedNRIC, tokenID, deviceID))
-                    {
-                        if (HttpContext.Current.Cache[retrievedNRIC + "_MFAAttempt"] != null &&
-                            HttpContext.Current.Cache.Get(retrievedNRIC + "_MFAAttempt").ToString().Equals("Awaiting"))
-                        {
-                            // MFA login was triggered, cache inserted
-                            Account account = new Account();
-                            account.associatedDeviceID = deviceID;
-                            account.associatedTokenID = tokenID;
-                            HttpContext.Current.Cache.Insert(retrievedNRIC + "_MFAAttempt", account, null, DateTime.Now.AddSeconds(30), Cache.NoSlidingExpiration);
-
-                            string newJwt = jwtBll.UpdateJWT(jwt);
-                            response = Request.CreateResponse(HttpStatusCode.OK, newJwt);
-                        }
-                        else
-                        {
-                            // MFA login was not trigger before this request, cache not inserted
-                            response = Request.CreateResponse(HttpStatusCode.NotFound);
-                        }
-                    }
-                }
-            }
-
-            /*if (HttpContext.Current.Cache[guid] != null)
-            {             
-                string nric = HttpContext.Current.Cache[guid].ToString();
-
-                // validate deviceID and tokenID for a guid
-                if (new AccountBLL().IsValid(nric, tokenID, deviceID))
-                {
-                    if (HttpContext.Current.Cache[nric + "_MFAAttempt"] != null &&
-                        HttpContext.Current.Cache.Get(nric + "_MFAAttempt").ToString().Equals("Awaiting"))
-                    {
-                        // MFA login was triggered, cache inserted
-                        Account account = new Account();
-                        account.associatedDeviceID = deviceID;
-                        account.associatedTokenID = tokenID;
-                        HttpContext.Current.Cache.Insert(nric + "_MFAAttempt", account, null, DateTime.Now.AddSeconds(30), Cache.NoSlidingExpiration);
-
-                        response = Request.CreateResponse(HttpStatusCode.OK);
-                    }
-                    else
-                    {
-                        // MFA login was not trigger before this request, cache not inserted
-                        response = Request.CreateResponse(HttpStatusCode.NotFound);
-                    }
-                }
-            }*/
-
-            return response;
-        }
-
-        [Route("weblogintest")]
-        [HttpPost]
-        public HttpResponseMessage WebLogintest()
+        public HttpResponseMessage WebLogin()
         {
             var response = Request.CreateResponse(HttpStatusCode.Unauthorized);
             string jwt;
             string deviceID;
             string tokenID;
+            string retrievedNRIC;
+
+            JWTBLL jwtBll = new JWTBLL();
 
             HttpContext httpContext = HttpContext.Current;
             string authHeader = httpContext.Request.Headers["Authorization"];
 
+            // Ensure Authorization Header exists
             if (authHeader != null && authHeader.StartsWith("Bearer"))
             {
                 string authHeaderValue = authHeader.Substring("Basic ".Length).Trim();
@@ -350,36 +284,42 @@ namespace NUSMed_WebApp.API
                 return response;
             }
 
-            JWTBLL jwtBll = new JWTBLL();
-
-            if (!string.IsNullOrEmpty(jwt) && AccountBLL.IsDeviceIDValid(deviceID) && AccountBLL.IsTokenIDValid(tokenID))
+            // Ensure jwt, deviceID, tokenID exists
+            if (!(!string.IsNullOrEmpty(jwt) && AccountBLL.IsDeviceIDValid(deviceID) && AccountBLL.IsTokenIDValid(tokenID)))
             {
-                if (jwtBll.ValidateJWT(jwt))
+                return response;
+            }
+
+            // Validate jwt
+            if (!jwtBll.ValidateJWT(jwt))
+            {
+                return response;
+            }
+            else
+            {
+                retrievedNRIC = jwtBll.getNRIC(jwt);
+            }
+
+            // Validate deviceID and tokenID for retrievedNRIC
+            if (accountBLL.IsValid(retrievedNRIC, tokenID, deviceID))
+            {
+                if (HttpContext.Current.Cache[retrievedNRIC + "_MFAAttempt"] != null &&
+                    HttpContext.Current.Cache.Get(retrievedNRIC + "_MFAAttempt").ToString().Equals("Awaiting"))
                 {
-                    string retrievedNRIC = jwtBll.getNRIC(jwt);
+                    // MFA login was triggered, cache inserted
+                    Account account = new Account();
+                    account.associatedDeviceID = deviceID;
+                    account.associatedTokenID = tokenID;
+                    HttpContext.Current.Cache.Insert(retrievedNRIC + "_MFAAttempt", account, null, DateTime.Now.AddSeconds(30), Cache.NoSlidingExpiration);
 
-                    // validate deviceID and tokenID for a nric
-                    if (accountBLL.IsValid(retrievedNRIC, tokenID, deviceID))
-                    {
-                        if (HttpContext.Current.Cache[retrievedNRIC + "_MFAAttempt"] != null &&
-                            HttpContext.Current.Cache.Get(retrievedNRIC + "_MFAAttempt").ToString().Equals("Awaiting"))
-                        {
-                            // MFA login was triggered, cache inserted
-                            Account account = new Account();
-                            account.associatedDeviceID = deviceID;
-                            account.associatedTokenID = tokenID;
-                            HttpContext.Current.Cache.Insert(retrievedNRIC + "_MFAAttempt", account, null, DateTime.Now.AddSeconds(30), Cache.NoSlidingExpiration);
-
-                            string newJwt = jwtBll.UpdateJWT(jwt);
-                            response = Request.CreateResponse(HttpStatusCode.OK);
-                            response.Headers.Add("Authorization", "Bearer " + newJwt);
-                        }
-                        else
-                        {
-                            // MFA login was not trigger before this request, cache not inserted
-                            response = Request.CreateResponse(HttpStatusCode.NotFound);
-                        }
-                    }
+                    string newJwt = jwtBll.UpdateJWT(jwt);
+                    response = Request.CreateResponse(HttpStatusCode.OK);
+                    response.Headers.Add("Authorization", "Bearer " + newJwt);
+                }
+                else
+                {
+                    // MFA login was not triggered before this request, cache not inserted
+                    response = Request.CreateResponse(HttpStatusCode.BadRequest);
                 }
             }
 
